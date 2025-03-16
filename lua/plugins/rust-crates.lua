9e67a3d132540cd466c28021e556aac226408555
Pratik <pmgajjar25@gmail.com>
2025-03-17 01:49:33 +0530

    feat(ai): avante

--- lua/plugins/rust-crates.lua
+++ lua/plugins/rust-crates.lua
@@ -1,19 +1,9 @@
 local M = {
   "saecki/crates.nvim",
+  tag = "stable",
   event = { "BufRead Cargo.toml" },
   requires = { { "nvim-lua/plenary.nvim" } },
   ft = { "rust", "toml" },
 }
-
-function M.config()
-  local crates = require("crates")
-  crates.setup({
-    -- null_ls = {
-    --   enabled = true,
-    --   name = "crates.nvim",
-    -- },
-  })
-  crates.show()
-end
 
 return M
